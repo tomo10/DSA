3bc5931d0b66d1877e4400654ff355f52c3dd33a
tomo <thce28@gmail.com>
2024-01-25 14:02:47 +0000

    Linked list now a doubly linked list

--- createLinkedList.js
+++ createLinkedList.js
@@ -17,20 +17,24 @@ class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
+    this.previous = null;
   }
 }
 
-class LinkedList {
+class DoublyLinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      next: null
+      next: null,
+      previous: null
     }
     this.tail = this.head;
     this.length = 1;
   }
   append(value) {
     const newNode = new Node(value)
+    newNode.previous = this.tail;
+
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
@@ -39,7 +43,9 @@ class LinkedList {
 
   prepend(value) {
     const newNode = new Node(value)
+
     newNode.next = this.head;
+    this.head.previous = newNode;
     this.head = newNode
     this.length++;
     return this;
@@ -56,7 +62,7 @@ class LinkedList {
     return array;
   }
 
-  insert(value, index) {
+  insert(index, value) {
     if (index >= this.length) {
       return this.append(value);
     }
@@ -64,10 +70,14 @@ class LinkedList {
 
     // want to get to the node (leader) just before the index we are given
     const leader = this.traverseToIndex(index - 1);
-    const holdingPointer = leader.next;
+    const follower = leader.next;
+
     leader.next = newNode;
-    newNode.next = holdingPointer;
+    newNode.previous = leader;
+    newNode.next = follower;
+    follower.previous = newNode;
     this.length++
+    console.log(this)
     // return printList() just for demo
     return this.printList()
   }
@@ -83,20 +93,30 @@ class LinkedList {
   }
 
   remove(index) {
-    // check params
+    // check params (we havent coded this)
     const leader = this.traverseToIndex(index-1);
     const unwantedNode = leader.next;
     leader.next = unwantedNode.next;
+    unwantedNode.previous = leader;
+
     this.length--;
     return this.printList()
   }
 }
 
-const myLinkedList = new LinkedList(10)
+const myLinkedList = new DoublyLinkedList(10)
 
-myLinkedList.prepend(91)
-myLinkedList.prepend(5)
-myLinkedList.prepend(1)
-myLinkedList.insert(44, 2)
-myLinkedList.insert(123, 7)
-myLinkedList.remove(2)
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(1,99);
+myLinkedList.printList();
+myLinkedList.remove(1); // should remove 10 from the list
+console.log(myLinkedList);
+
+// myLinkedList.prepend(91)
+// myLinkedList.prepend(5)
+// myLinkedList.prepend(1)
+// myLinkedList.insert(44, 2)
+// myLinkedList.insert(123, 7)
+// myLinkedList.remove(2)
